d33d8f786f898752d526783c8790686b269edc87
carlcheerio <116285555+carlcheerio@users.noreply.github.com>
2022-10-20 09:52:12 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+let bullets = []
+
+function setup() {
+  createCanvas(400, 400);
+}
+function draw() {
+  background(51);
+  //draw the player
+    circle(mouseX,height-50,25);
+  for (let bullet of bullets){
+    circle(bullet.x, bullet.y, 10)
+    bullet.y -= 2
+  }
+}
+
+function mousePressed() {
+let bullet = {
+  x: mouseX, 
+  y: height-50
+  }
+bullets.push(bullet)
+}
+//bullets spawn on click
+//enemies
+//collisions
+//score
+//lose game
